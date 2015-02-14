5e9addfcc4b4d2c42970b4537cf7613f75e13f9f
Justyna Stebel <justyna.stebel@gmail.com>
2015-02-14 13:13:36 +0100

    Test problems solved

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,11 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }, allow_blank: true
 
 
-  # Returns the hash digest of the given string.
+ # Returns the hash digest of the given string.
   def User.digest(string)
-    Digest::SHA1.hexdigest(string.to_s)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
   end
 
   # Returns a random token.
